33682e5975348a004fdf18a364299e3d951607c4
Abhinav M <46239775+AbhinavM2000@users.noreply.github.com>
2021-12-03 15:42:24 +0530

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -11,7 +11,7 @@ export default function MyComponent(props) {
           <div className="builder-columns columns">
             <div className="builder-column column">
               <div className="div-4">
-                <p style={{'color':'red','--hover-color':'blue','--hover-opacity':0.5}}><a href="url">HOME</a></p>
+                <a style={{'color':'red','--hover-color':'blue','--hover-opacity':0.5}}> href="url">HOME</a>
               </div>
             </div>
 
