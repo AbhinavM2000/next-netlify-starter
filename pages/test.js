aca608da6525760cdd617a8b83d639afc8bc8322
Abhinav M <46239775+AbhinavM2000@users.noreply.github.com>
2021-12-03 10:28:34 +0530

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -1,57 +1,141 @@
-import './xx.module.css'
-
-
 export default function MyComponent(props) {
   return (
     <>
-<div id="maincontainer">     
-<div id="headermainheader"></div>
-<div class="left"></div>
-<div class="stuff">
-
-  <h1>Resume</h1>
-  <h2>Emily</h2>
-  <hr />
- 
-  <p class="head">Interests</p>
-  <ul>
-    <li>Drawing</li>
-    <li>Photography</li>
-    <li>Design</li>
-    <li>Programming</li>
-    <li>Computer Science</li>
-  </ul>
-  <p class="head">Skills</p>
-  <ul>
-    <li>Web Design with HTML & CSS</li>
-  </ul>
-  <p class="head">Education</p>
-  <ul>
-    <a href="http://www.wiltonhighschool.org/pages/Wilton_High_School">
-      <li>Wilton High School</li>
-    </a>
- 
-    <a href="https://www.silvermineart.org/">
-      <li>Silvermine School of Arts</li>
-    </a>
-    <li>Codeacademy</li>
-  </ul>
-  <p class="head">Experience</p>
-  <ul>
-    <li>Student Technology Intern for Wilton School District</li>
-    <li>Babysitter</li>
-  </ul>
-  <p class="head">Extracurriculars</p>
-  <ul>
-    <li>Recycling Club</li>
-    <li>Gardening Club</li>
-    <li>Book Club</li>
-  </ul>
-</div>
-<div class="right"></div>
-<div id="footer">
-  <div id="name">Emily</h2></div>
-</div>
+      <div className="div">
+        <p>
+          <strong>
+            <em>Curriculum vitae</em>
+          </strong>
+          <span class="ql-cursor">﻿</span>
+        </p>
+      </div>
+      <div className="div-2" maxWidth={1200}>
+        <section className="section">
+          <p>
+            <b>
+              I am a section! My content keeps from getting too wide, so that
+              it's easy to read even on big screens.
+            </b>
+          </p>{" "}
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur
+          </p>{" "}
+        </section>
+      </div>{" "}
+      <div className="div-3" maxWidth={1200}>
+        <section className="section">
+          {" "}
+          <p>
+            <strong>
+              I am a section! My content keeps from getting too wide, so that
+              it's easy to read even on big screens.
+            </strong>
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur
+          </p>
+          <p>0</p>
+          <p>0</p>
+          <p>0</p>
+          <p>0</p>
+          <p>0</p>
+          <p>0</p>
+          <p>0</p>
+          <p>0</p>
+          <p>0</p>
+          <p>0</p>
+        </section>
+      </div>
+      <style jsx>{`
+        .div {
+          display: flex;
+          flex-direction: column;
+          position: relative;
+          margin-top: 20px;
+          height: auto;
+          padding-top: 0px;
+          background-color: rgba(74, 74, 74, 1);
+          opacity: 1;
+          font-family: "sans-serif", sans-serif;
+          padding-bottom: 30px;
+          margin-bottom: -2px;
+          line-height: normal;
+          text-align: center;
+        }
+        @media (max-width: 991px) {
+          .div {
+            display: flex;
+            flex-direction: row;
+            overflow: auto;
+            background-color: rgba(224, 201, 166, 1);
+            padding-bottom: px;
+            margin-top: 0px;
+            width: 100vw;
+            align-self: stretch;
+            margin-left: auto;
+            margin-right: auto;
+          }
+        }
+        .div-2 {
+          display: flex;
+          flex-direction: column;
+          position: relative;
+          margin-top: 0px;
+          padding-left: 20px;
+          padding-right: 20px;
+          padding-top: 50px;
+          padding-bottom: 50px;
+          width: 100vw;
+          margin-left: calc(50% - 50vw);
+          background-color: rgba(251, 251, 248, 1);
+          text-align: center;
+        }
+        @media (max-width: 991px) {
+          .div-2 {
+            background-color: rgba(224, 201, 166, 1);
+          }
+        }
+        .section {
+          width: 100%;
+          align-self: stretch;
+          flex-grow: 1;
+          max-width: 1200px;
+          display: flex;
+          flex-direction: column;
+          margin-left: auto;
+          margin-right: auto;
+        }
+        .div-3 {
+          display: flex;
+          flex-direction: column;
+          position: relative;
+          margin-top: 0px;
+          padding-left: 20px;
+          padding-right: 20px;
+          padding-top: 50px;
+          padding-bottom: 50px;
+          width: 100vw;
+          margin-left: calc(50% - 50vw);
+          text-align: center;
+        }
+        @media (max-width: 991px) {
+          .div-3 {
+            margin-bottom: 0px;
+            padding-bottom: 35px;
+            background-color: rgba(224, 201, 166, 1);
+          }
+        }
+      `}</style>
     </>
   );
 }
