be3f3a61ee8517f396e205e7ffbfab8e7d35c14f
Abhinav M <46239775+AbhinavM2000@users.noreply.github.com>
2021-12-07 03:24:13 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,11 @@ export default function Home() {
         <Header title="I need to set this up !" />
        
 <Link href='https://abnv.ml'> 
-<p>So many things to do... so little time.</p>
+<p class='description'>So many things to do... so little time.</p>
+</Link> 
+    
+    <Link href='https://abnv.ml'> 
+<a>Here is my resume for now.</a>
 </Link> 
       </main>
 
