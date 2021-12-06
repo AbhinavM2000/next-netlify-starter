4f52f5330649bea5f22ab3f043a4e16d3ca36d5a
Abhinav M <46239775+AbhinavM2000@users.noreply.github.com>
2021-12-07 03:30:01 +0530

    Update cv.js

--- pages/cv.js
+++ pages/cv.js
@@ -1,36 +1,11 @@
-import React from 'react';
+import dynamic from 'next/dynamic';
 
-import Article from '../components/Article';
-import PDFLayout from '../components/PDFLayout';
-import pdfHelper from '../lib/pdfHelper';
+const FileViewer = dynamic(() => import('react-file-viewer'), {
+    ssr: false
+});
 
-class IndexPage extends React.Component {
-  static async getInitialProps({ req, res, query }) {
-    const exportPDF = query.exportPDF === 'true';
-    const isServer = !!req;
-
-    if (isServer && exportPDF) {
-      const buffer = await pdfHelper.componentToPDFBuffer(
-        <PDFLayout><Article/></PDFLayout>
-      );
-
-      // with this header, your browser will prompt you to download the file
-      // without this header, your browse will open the pdf directly
-      res.setHeader('Content-disposition', 'attachment; filename="article.pdf');
-      
-      // set content type
-      res.setHeader('Content-Type', 'application/pdf');
-
-      // output the pdf buffer. once res.end is triggered, it won't trigger the render method
-      res.end(buffer);
-    }
-
-    return {};
-  }
-
-  render() {
-    return (<Article/>)
-  }
-}
-
-export default IndexPage;
+export default function Index() {
+    return (
+        <FileViewer fileType="pdf" filePath="/pdf.pdf" />
+    );
+};
