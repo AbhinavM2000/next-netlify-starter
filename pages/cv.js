8bc9fcc6bc9024932bf1d0a953676efc9819631d
Abhinav M <46239775+AbhinavM2000@users.noreply.github.com>
2021-12-07 03:43:23 +0530

    Update cv.js

--- pages/cv.js
+++ pages/cv.js
@@ -1,56 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const props = {
-  allowFullScreen: true,
-  src: "CV.pdf"
-};
+import  React,{  Component } from 'react';
+import ReactDOM from 'react-dom';
 
 
+class DocView extends React.Component{
 
+    constructor(props){
+        super(props);
+    }
 
-function App() {
-  return (
-    <Modal
-aria-labelledby='transition-modal-title'
-aria-describedby='transition-modal-description'
-className={classes.modal}
-open={open}
-onClose={handleClose}
-closeAfterTransition
-BackdropComponent={Backdrop}
-BackdropProps={{
-	timeout: 500,
-}}>
-<Fade in={open}>
-	<div
-		className={classes.paper}
-		style={{ width: '70%', height: '90%' }}>
-		<Grid
-			container
-			direction='row'
-			justify='space-between'
-			alignItems='flex-start'
-			style={{ marginBottom: '10px' }}>
-			<Grid>
-				<h4 id='transition-modal-title'>
-				</h4>
-		        </Grid>
-		</Grid>
-		<p id='transition-modal-description'
-		style={{ width: '100%', height: '95%' }}>
-		<embed
-		style={{
-		        width: '100%',
-			height: '100%',
-		}}
-		type='application/pdf'
-		src={pdf}
-		/>
-		</p>
-	</div>
-</Fade>
-</Modal>
-  );
+    render(){
+        var url = "https://docs.google.com/viewerng/viewer?url=https://github.com/AbhinavM2000/next-netlify-starter/blob/main/pages/cv.js"+this.props.src+"&embedded=true";
+        return(
+            <iframe style={this.props.style} src={url}></iframe>
+        );
+    }
 }
 
+export default DocView;
