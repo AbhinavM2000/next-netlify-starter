9e61ac2c251c57960223970bd81aebed0cfe9d5c
Abhinav M <46239775+AbhinavM2000@users.noreply.github.com>
2021-12-07 04:01:05 +0530

    Update cv.js

--- pages/cv.js
+++ pages/cv.js
@@ -11,7 +11,7 @@ class DocView extends React.Component{
     render(){
         var url = "https://docs.google.com/viewerng/viewer?url=https://github.com/AbhinavM2000/next-netlify-starter/raw/main/pages/CV.pdf&embedded=true";
         return(
-            <iframe height="1280px" width="720px" src={url}></iframe>
+         <div align="center">   <iframe height="1280px" width="720px" src={url}></iframe></div>
         );
     }
 }
