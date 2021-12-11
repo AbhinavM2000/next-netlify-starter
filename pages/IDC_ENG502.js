45ed3feca8373ef6e522632e784786bfce40742f
Abhinav M <46239775+AbhinavM2000@users.noreply.github.com>
2021-12-11 07:28:48 +0530

    Update IDC_ENG502.js

--- pages/IDC_ENG502.js
+++ pages/IDC_ENG502.js
@@ -10,25 +10,25 @@ export default function Home() {
 
 <p><strong>IDC_ENG502</strong></p>
  <Link href='https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/lit%20and%20sci%20matt%20ar.pdf'> 
-<a>Matthew Arnold: Literature and Science<br/></a>
+<a>Matthew Arnold: Literature and Science<br/><br/></a>
 </Link> 
 
 </div><div className="idc">
 <Link href='https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/huxx.pdf'> 
-<a>T.H. Huxley: Science and Culture</a>
+<a>T.H. Huxley: Science and Culture<br/><br/></a>
 </Link> 
 </div> <div className="idc">
 
     <Link href='https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/khun.pdf'> 
-<a>T.S. Kuhn: The Nature and Necessity of Scientific Revolution</a>
+<a>T.S. Kuhn: The Nature and Necessity of Scientific Revolution<br/><br/></a>
 </Link> 
 </div><div className="idc">
     <Link href='https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/snow_1959.pdf'> 
-<a>C.P. Snow: The Two Cultures </a>
+<a>C.P. Snow: The Two Cultures <br/><br/></a>
 </Link> 
 </div><div className="idc">
     <Link href='https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/poe.pdf'> 
-<a>Edgar Allan Poe: To Science</a>
+<a>Edgar Allan Poe: To Science<br/><br/></a>
 </Link> 
 </div><div className="idc">
     <Link href="https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/RICHARD%20P.%20FEYNMAN-SURELY%20YOU'RE%20JOKING%20MR.%20FEYNMAN.PDF"> 
@@ -36,7 +36,7 @@ export default function Home() {
 </Link>
    </div> <div className="idc">
     <Link href='https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/Connie%20Willis%20-%20Bellwether%20%20-Spectra%20(2009).pdf'> 
-<a>Connie Willis: Bellwether </a>
+<a>Connie Willis: Bellwether <br/><br/></a>
 </Link> 
 </div>
 </div>
