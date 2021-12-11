b419857a7353aba78195affb7a5d84e9b572ca3f
Abhinav M <46239775+AbhinavM2000@users.noreply.github.com>
2021-12-11 07:04:52 +0530

    Update IDC_ENG502.js

--- pages/IDC_ENG502.js
+++ pages/IDC_ENG502.js
@@ -35,7 +35,7 @@ export default function Home() {
 </Link> 
 
     <Link href="https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/RICHARD%20P.%20FEYNMAN-SURELY%20YOU'RE%20JOKING%20MR.%20FEYNMAN.PDF"> 
-<a>Richard Feynman: Surely You’re Joking Mr Feynman<br></a>
+<a>Richard Feynman: Surely You’re Joking Mr Feynman\n</a>
 </Link>
     
     <Link href='https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/Connie%20Willis%20-%20Bellwether%20%20-Spectra%20(2009).pdf'> 
