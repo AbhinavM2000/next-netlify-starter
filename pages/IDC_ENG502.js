b94925e2601a7ceb15a6a62b4e1292a917f49f1f
Abhinav M <46239775+AbhinavM2000@users.noreply.github.com>
2021-12-11 07:51:15 +0530

    Update IDC_ENG502.js

--- pages/IDC_ENG502.js
+++ pages/IDC_ENG502.js
@@ -10,7 +10,7 @@ export default function Home() {
 
 <p><strong>IDC_ENG502</strong></p><p><br/></p><hr>
  <Link href='https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/lit%20and%20sci%20matt%20ar.pdf'> 
-<p><strong>Matthew Arnold<strong/>: Literature and Science<br/><br/></p>
+<p><strong>Matthew Arnold<strong/></p><p>: Literature and Science<br/><br/></p>
 </Link> 
 
 </div><div className="idc">
