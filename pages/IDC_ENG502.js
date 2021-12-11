ed8dc2a385b0f55bc871d63da48f3833cdaf32c1
Abhinav M <46239775+AbhinavM2000@users.noreply.github.com>
2021-12-11 15:26:16 +0530

    Update IDC_ENG502.js

--- pages/IDC_ENG502.js
+++ pages/IDC_ENG502.js
@@ -9,37 +9,37 @@ export default function Home() {
 
 
 <p><strong>IDC_ENG502 --PDFs</strong></p><hr/><br/>
- <Link href='https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/lit%20and%20sci%20matt%20ar.pdf' download> 
+ <Link href='https://mega.nz/file/W2pBRKYJ#Aj31WuNKXi3Lh0-Ux3bzJ81sPbc92-9w01o7HSyfzAo' download> 
    <a><strong>Matthew Arnold : </strong>Literature and Science <br/><br/><br/></a>
 </Link> 
 
 </div><div className="idc">
-<Link href='https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/huxx.pdf'> 
+<Link href='https://mega.nz/file/v3AAET6S#1B-FjeVOeMvkb_aLDWo2nmldW6WmhHjwLxAI5pAs9C0'> 
     <a><strong>T.H. Huxley : </strong>Science and Culture <br/><br/><br/></a>
 </Link> 
 </div> <div className="idc">
 
-    <Link href='https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/khun.pdf'> 
+    <Link href='https://mega.nz/file/6zx3DaDB#O38Q-SNLXhFF1rDvgDuB3rkKPHhLdmng5dJBTum8hwI'> 
 <a><strong>T.S. Kuhn : </strong>The Nature and Necessity of Scientific Revolution<br/><br/><br/></a>
 </Link> 
 </div><div className="idc">
-    <Link href='https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/snow_1959.pdf'> 
+    <Link href='https://mega.nz/file/26YEkBrZ#KDvmugZxJUhSEHJTPuquoPqobEhF7znZy-Ihxoudrj8'> 
 <a><strong>C.P. Snow : </strong>The Two Cultures <br/><br/><br/></a>
 </Link> 
 </div><div className="idc">
-    <Link href='https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/poe.pdf'> 
+    <Link href='https://mega.nz/file/GywTEKTR#vVJs04hZ_glZmVx6-JvVjTEyUoamlcnJStHCDLebxNk'> 
 <a><strong>Edgar Allan Poe : </strong>To Science<br/><br/><br/></a>
 </Link> 
 </div><div className="idc">
-    <Link href="https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/RICHARD%20P.%20FEYNMAN-SURELY%20YOU'RE%20JOKING%20MR.%20FEYNMAN.PDF"> 
+    <Link href="https://mega.nz/file/T7AEQbhb#iEY7cWJ1su54rmMgxDH6s6ptQFKC_vcQmEMsnCJGAdg"> 
 <a><strong>Richard Feynman : </strong>Surely You’re Joking Mr Feynman <br/><br/><br/></a>
 </Link>
    </div> <div className="idc">
-    <Link href='https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/Connie%20Willis%20-%20Bellwether%20%20-Spectra%20(2009).pdf'> 
+    <Link href='https://mega.nz/file/LiBkyLwI#qqDOJ_xJlSpWEd3H9hlx7cCwtbWxU3YD8gZ6oxwEPK8'> 
 <a><strong>Connie Willis : </strong>Bellwether <br/><br/><br/></a>
 </Link> 
     <hr/>
-<br/><br/><br/><br/><p>Note: Downloaded files may not open in browser, go to downloads folder instead.</p>
+
 </div>
 </div>
 
