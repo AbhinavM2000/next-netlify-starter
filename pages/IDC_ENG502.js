fab236053b0f88f3e0467db8c719c251ab11e0ce
Abhinav M <46239775+AbhinavM2000@users.noreply.github.com>
2021-12-11 07:53:06 +0530

    Update IDC_ENG502.js

--- pages/IDC_ENG502.js
+++ pages/IDC_ENG502.js
@@ -10,33 +10,33 @@ export default function Home() {
 
 <p><strong>IDC_ENG502</strong></p><p><br/></p><hr>
  <Link href='https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/lit%20and%20sci%20matt%20ar.pdf'> 
-<p><strong>Matthew Arnold<strong/></p><p>: Literature and Science<br/><br/></p>
+<p><strong>Matthew Arnold</strong></p><p>: Literature and Science<br/><br/></p>
 </Link> 
 
 </div><div className="idc">
 <Link href='https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/huxx.pdf'> 
-<p><strong>T.H. Huxley<strong/>: Science and Culture<br/><br/></p>
+<p><strong>T.H. Huxley</strong>: Science and Culture<br/><br/></p>
 </Link> 
 </div> <div className="idc">
 
     <Link href='https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/khun.pdf'> 
-<a><strong>T.S. Kuhn<strong/>: The Nature and Necessity of Scientific Revolution<br/><br/></a>
+<a><strong>T.S. Kuhn</strong>: The Nature and Necessity of Scientific Revolution<br/><br/></a>
 </Link> 
 </div><div className="idc">
     <Link href='https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/snow_1959.pdf'> 
-<a><strong>C.P. Snow<strong/>: The Two Cultures <br/><br/></a>
+<a><strong>C.P. Snow</strong>: The Two Cultures <br/><br/></a>
 </Link> 
 </div><div className="idc">
     <Link href='https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/poe.pdf'> 
-<a><strong>Edgar Allan Poe<strong/>: To Science<br/><br/></a>
+<a><strong>Edgar Allan Poe</strong>: To Science<br/><br/></a>
 </Link> 
 </div><div className="idc">
     <Link href="https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/RICHARD%20P.%20FEYNMAN-SURELY%20YOU'RE%20JOKING%20MR.%20FEYNMAN.PDF"> 
-<a><strong>Richard Feynman<strong/>: Surely You’re Joking Mr Feynman <br/><br/></a>
+<a><strong>Richard Feynman</strong>: Surely You’re Joking Mr Feynman <br/><br/></a>
 </Link>
    </div> <div className="idc">
     <Link href='https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/Connie%20Willis%20-%20Bellwether%20%20-Spectra%20(2009).pdf'> 
-<a><strong>Connie Willis<strong/>: Bellwether <br/><br/></a>
+<a><strong>Connie Willis</strong>: Bellwether <br/><br/></a>
 </Link> 
 </div>
 </div>
