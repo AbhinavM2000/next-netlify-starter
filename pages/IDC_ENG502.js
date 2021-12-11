06a4617b57894639ae1ef3ebbe64bdd3f56e039f
Abhinav M <46239775+AbhinavM2000@users.noreply.github.com>
2021-12-11 06:53:55 +0530

    Update IDC_ENG502.js

--- pages/IDC_ENG502.js
+++ pages/IDC_ENG502.js
@@ -16,30 +16,29 @@ export default function Home() {
  <Link href='https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/lit%20and%20sci%20matt%20ar.pdf'> 
 <a>Matthew Arnold: Literature and Science</a>
 </Link> 
-<br><br>
+
 <Link href='https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/huxx.pdf'> 
 <a>T.H. Huxley: Science and Culture</a>
 </Link> 
     
-<br><br>
+
     <Link href='https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/khun.pdf'> 
 <a>T.S. Kuhn: The Nature and Necessity of Scientific Revolution</a>
 </Link> 
-    <br><br>
+   
 
     <Link href='https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/snow_1959.pdf'> 
 <a>C.P. Snow: The Two Cultures </a>
 </Link> 
-<br><br>
+
     <Link href='https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/poe.pdf'> 
 <a>Edgar Allan Poe: To Science</a>
 </Link> 
  
-    <br><br>
     <Link href="https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/RICHARD%20P.%20FEYNMAN-SURELY%20YOU'RE%20JOKING%20MR.%20FEYNMAN.PDF"> 
 <a>Richard Feynman: Surely You’re Joking Mr Feynman</a>
 </Link> 
-<br><br>
+
     
     <Link href='https://github.com/AbhinavM2000/next-netlify-starter/raw/main/public/Connie%20Willis%20-%20Bellwether%20%20-Spectra%20(2009).pdf'> 
 <a>Connie Willis: Bellwether </a>
