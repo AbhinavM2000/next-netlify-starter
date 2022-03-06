7e03ad9a539823fdfbbc4ff52ef5e24bcc1ba7bd
Abhinav M <46239775+AbhinavM2000@users.noreply.github.com>
2022-03-06 14:08:18 +0530

    Create QM.js

---
+++ pages/QM.js
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+<div className="idc2">
+
+<div className="idc">
+
+
+<p><strong>IDC_ENG502 --PDFs</strong></p><hr/><br/>
+ <Link href='https://file.io/MlZH7VGNdMCp' download> 
+   <a><strong>Matthew Arnold : </strong>No nonsense QM for Aanchal Tyagi<br/><br/><br/></a>
+</Link> 
+
+
+    <hr/>
+
+</div>
+</div>
+
+  )
+}
