a0c4b42150feff9222112f5e0a9685573c771450
Abhinav M <46239775+AbhinavM2000@users.noreply.github.com>
2022-03-07 12:58:59 +0530

    Update QM.js

--- pages/QM.js
+++ pages/QM.js
@@ -1,8 +1,6 @@
 import Head from 'next/head'
 import Image from "next/image"
 import Link from 'next/link'
-import image1 from '/image1.gif';
-import image1 from 'image1.gif';
 export default function Home() {
   return (
 <div className="idc2">
@@ -15,11 +13,11 @@ export default function Home() {
    <a><strong>No nonsense QM for Aanchal Tyagi</strong><br/><br/><br/></a>
 </Link> 
 
-<Image src={image1} />
+
     <hr/>
 
 
-<Image src={image1} />
+<img src="/image1.gif" alt="Netlify Logo" />
 
 
 
