f64ed282a1492d6e894337d0227bdbb4bb0baf93
Abhinav M <46239775+AbhinavM2000@users.noreply.github.com>
2022-04-06 12:29:49 +0530

    Create tt.js

---
+++ pages/tt.js
@@ -0,0 +1,5 @@
+function View() {
+    return(
+       <iframe src="https://calendar.google.com/calendar/embed?src=87njf800jimk3skmo9vfrqrfq8%40group.calendar.google.com&ctz=Asia%2FKolkata" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+    )
+}
