b587a343ebc8ab47debc58b1808dfb5c9b6b34a5
Abhinav M <46239775+AbhinavM2000@users.noreply.github.com>
2022-04-06 12:47:27 +0530

    Delete tt.js

--- public/tt.js
+++
@@ -1,7 +0,0 @@
-function View() {
-    return(
-       <iframe src="https://calendar.google.com/calendar/embed?src=87njf800jimk3skmo9vfrqrfq8%40group.calendar.google.com&ctz=Asia%2FKolkata" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
-    )
-}
-
-
