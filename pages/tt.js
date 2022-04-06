1f33f7b97f8f75575b0ed722b97a506e7b243dd9
Abhinav M <46239775+AbhinavM2000@users.noreply.github.com>
2022-04-06 12:36:40 +0530

    Update tt.js

--- pages/tt.js
+++ pages/tt.js
@@ -1,12 +1,2 @@
 
-function App() {
-  return (
-    <div className="App">
-      <h3>Iframes in React</h3>
-     <iframe src="https://calendar.google.com/calendar/embed?src=87njf800jimk3skmo9vfrqrfq8%40group.calendar.google.com&ctz=Asia%2FKolkata" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
-    
-    </div>
-  );
-}
-
-export default App;
+  <Link to={{ pathname: "https://example.zendesk.com/hc/en-us/articles/123456789-Privacy-Policies" }} target="_blank" />
